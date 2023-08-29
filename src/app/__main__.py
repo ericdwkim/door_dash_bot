b40eb2e08a4dfedefcca991f54cc4457d8fac5d0
ericdwkim <ericdwkim.io@gmail.com>
2023-08-29 14:36:43 -0500

    test: main.wrapper to launch, login, and switch to orders page

--- src/app/__main__.py
+++ src/app/__main__.py
@@ -8,17 +8,38 @@ class Main:
         # @dev: subclass drivers have and use base_driver
         self.base_driver = BaseDriver(headless=headless)
         self.login_page_driver = LoginPageDriver(self.base_driver)
-        # self.home_page_driver = HomePageDriver(self.base_driver)
+        self.home_page_driver = HomePageDriver(self.base_driver)
         # self.online_reports_driver = OnlineReportsPageDriver(self.base_driver)
 
     def launch_and_login_to_door_dash(self):
-        login_to_cash = self.login_page_driver.visit_and_login()
-        if not login_to_cash:
+        login_to_dash = self.login_page_driver.visit_and_login()
+        if not login_to_dash:
             logging.error('Could not login_page_driver.launch_and_login_to_door_dash')
             return False
         else:
             logging.info('Successfully launched and logged into Door Dash.')
             return True
+
+    def switch_to_orders_page(self):
+        switched_to_orders_page = self.home_page_driver.switch_to_orders_page()
+        if not switched_to_orders_page:
+            logging.error('Could not switch_to_orders_page')
+            return False
+        else:
+            logging.info('Successfully switched to orders page from homepage')
+            return True
+
+
+    def wrapper(self):
+        try:
+            launched_and_logged_into_door_dash_homepage = self.launch_and_login_to_door_dash()
+            if not launched_and_logged_into_door_dash_homepage:
+                logging.error('Could not launch_and_login_to_door_dash')
+            switched_to_orders_page = self.switch_to_orders_page()
+            if not switched_to_orders_page:
+                loggging.error('Could not switch_to_orders_page in wrapper')
+        except Exception as e:
+            logging.exception(f'An error occurred attempting to launch, login, and switch from home to orders page: {e}')
 
 
 if __name__ == '__main__':
@@ -28,7 +49,7 @@ if __name__ == '__main__':
 
     md = Main(headless=args.headless)
 
-    md.launch_and_login_to_door_dash()
+    md.wrapper()
 
 
 
