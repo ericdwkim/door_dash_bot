80e90492170fc01c6e37ae3edcefadd319092ca6
ericdwkim <ericdwkim.io@gmail.com>
2023-08-29 14:29:02 -0500

    add: home and orders page templates

---
+++ src/pages/homepage.py
@@ -0,0 +1,26 @@
+import time
+import logging
+from src.pages.basepage import BasePage
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+class HomePage(BasePage):
+    def __init__(self, driver):
+        super().__init__(driver)
+
+
+    def switch_to_orders_page(self):
+
+        try:
+            is_element_clicked = self.wait_for_find_then_click('//*[@id="Reporting_TreeItem_div"]/td[2]/table/tbody/tr[2]/td[2]/a', locator_type=By.XPATH)
+            if not is_element_clicked:
+                logging.error(f'Could not switch to Online Reports')
+                return False
+            else:
+                return True
+
+        except Exception as e:
+            logging.exception(f'An error occurred trying to drill into Online Reports: {e}')
+            return False
+
