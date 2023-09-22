c6ad3bd363e820a33fd971e2e3b80e38cf33004e
ericdwkim <ericdwkim.io@gmail.com>
2023-09-21 20:59:37 -0500

    add: test.py - oop scratchpad from procedural to avoid debugging second browser instance issue in __main__.py. does not launch second instance and controls target as expected.

---
+++ src/app/test.py
@@ -0,0 +1,124 @@
+from selenium import webdriver
+from selenium.webdriver import ChromeOptions
+from selenium.webdriver.chrome.service import Service
+from src.pages.basepage import BasePage
+import os
+import logging
+from selenium.webdriver.common.by import By
+
+
+class TestDriver:
+
+    def __init__(self):
+        # self.url = os.getenv('DD_MERCHANT_LOGIN_URL')
+        self.setup_driver()
+
+    def setup_driver(self):
+        options = self._get_chrome_options()
+        chromedriver_exe_path = '/opt/homebrew/bin/chromedriver'
+
+
+        self.driver = webdriver.Chrome(
+            service=Service(executable_path=chromedriver_exe_path),
+            options = options
+        )
+
+    def _get_chrome_options(self):
+        options = ChromeOptions()
+        options.add_argument('--remote-debugging-port=9222')
+
+        return options
+
+
+class LoginPage(BasePage):
+
+    def __init__(self, base_driver):
+        super().__init__(base_driver)
+        self.url = os.getenv('DD_MERCHANT_LOGIN_URL')
+        self.username = os.getenv('DEV_LOGIN_EMAIL')
+        self.password = os.getenv('DEV_LOGIN_PASSWORD')
+
+    def visit(self):
+        """
+        launch browser and navigate to URL
+        :return: bool
+        """
+        try:
+            logging.info('Launching DoorDash Merchant Portal....')
+            self.driver.get(self.url)
+            return True
+        except Exception as e:
+            logging.exception(f'Browser could not be launched\nAn error occurred trying to visit "{self.url}". Error message: {e}')
+
+    def enter_username(self):
+        try:
+            is_element_present, was_clicked, element_selector_clicked = self.wait_for_find_then_click_then_send_keys('//*[@id="FieldWrapper-0"]', keys_to_send=self.username, locator_type=By.XPATH, timeout=10)
+            if not is_element_present and not was_clicked and not element_selector_clicked:
+                return False, False, None
+            else:
+                return True, True, element_selector_clicked
+        except Exception as e:
+            logging.exception(f'An error occurred trying to enter_username: {e}')
+            return False, False, None
+
+    def enter_password(self):
+        try:
+            is_element_present, was_clicked, element_selector_clicked = self.wait_for_find_then_click_then_send_keys('//*[@id="FieldWrapper-1"]', keys_to_send=self.password, locator_type=By.XPATH, timeout=10)
+            if not is_element_present and not was_clicked and not element_selector_clicked:
+                return False, False, None
+            else:
+                return True, True, element_selector_clicked
+        except Exception as e:
+            logging.exception(f'An error occurred trying to enter_username: {e}')
+            return False, False, None
+
+    def click_login_button(self):
+        try:
+            was_clicked = self.wait_for_find_then_click('//*[@id="login-submit-button"]', locator_type=By.XPATH, timeout=10)
+            if not was_clicked:
+                logging.error('Could not click_login_button')
+                return False
+            else:
+                return True
+        except Exception as e:
+            logging.exception(f'An error occurred trying to click_login_button: {e}')
+            return False
+
+    def login(self):
+        try:
+            username_entered = self.enter_username()
+            password_entered = self.enter_password()
+            login_btn_clicked = self.click_login_button()
+            if username_entered and password_entered and login_btn_clicked:
+                logging.info('Successfully logged in!')
+                return True
+            else:
+                logging.error(f'Could not login with provided credentials')
+                return False
+        except Exception as e:
+            logging.exception(f'An error occurred trying to login: {e}')
+            return False
+
+    def visit_and_login(self):
+        try:
+            if not self.visit():
+                return False
+            if not self.login():
+                return False
+            else:
+                logging.info('Browser launched successfully!')
+                return True
+        except Exception as e:
+            logging.exception(f'An error occurred trying to visit_and_login: {e}')
+
+
+class Main:
+    def __init__(self):
+        self.base_driver = TestDriver()
+        self.login_page_driver = LoginPage(self.base_driver)
+
+
+        self.login_page_driver.visit_and_login()
+
+
+m = Main()
