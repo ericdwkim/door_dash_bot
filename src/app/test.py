fa2e22b273a38765ce577da94b7fb1ecd936c332
ericdwkim <ericdwkim.io@gmail.com>
2023-09-22 01:25:39 -0500

    add: OrdersPage in test.py to test visiting and `switch_to_history_tab`. todo: TestDriver missing `find_element` attributes and others.

--- src/app/test.py
+++ src/app/test.py
@@ -10,7 +10,7 @@ from selenium.webdriver.common.by import By
 class TestDriver:
 
     def __init__(self):
-        # self.url = os.getenv('DD_MERCHANT_LOGIN_URL')
+        self.url = os.getenv('DD_MERCHANT_LOGIN_URL')
         self.setup_driver()
 
     def setup_driver(self):
@@ -29,37 +29,10 @@ class TestDriver:
 
         return options
 
-
-class LoginPage(BasePage):
-
-    def __init__(self, base_driver):
-        super().__init__(base_driver)
-        self.url = os.getenv('DD_MERCHANT_LOGIN_URL')
-        self.username = os.getenv('DEV_LOGIN_EMAIL')
-        self.password = os.getenv('DEV_LOGIN_PASSWORD')
-
     def visit(self):
-        """
-        launch browser and navigate to URL
-        :return: bool
-        """
-        try:
-            logging.info('Launching DoorDash Merchant Portal....')
-            self.driver.get(self.url)
-            return True
-        except Exception as e:
-            logging.exception(f'Browser could not be launched\nAn error occurred trying to visit "{self.url}". Error message: {e}')
+        self.driver.get(self.url)
+        print(f'=----------------------------')
 
-    def enter_username(self):
-        try:
-            is_element_present, was_clicked, element_selector_clicked = self.wait_for_find_then_click_then_send_keys('//*[@id="FieldWrapper-0"]', keys_to_send=self.username, locator_type=By.XPATH, timeout=10)
-            if not is_element_present and not was_clicked and not element_selector_clicked:
-                return False, False, None
-            else:
-                return True, True, element_selector_clicked
-        except Exception as e:
-            logging.exception(f'An error occurred trying to enter_username: {e}')
-            return False, False, None
 
 class OrdersPage(BasePage):
 
@@ -68,51 +41,26 @@ class OrdersPage(BasePage):
     def switch_to_history_tab(self):
 
         try:
-            was_clicked = self.wait_for_find_then_click('//*[@id="login-submit-button"]', locator_type=By.XPATH, timeout=10)
-            if not was_clicked:
-                logging.error('Could not click_login_button')
+            is_element_clicked = self.wait_for_find_then_click(
+                '//*[@id="MerchantApp"]/div/div/div[1]/div/div[2]/div[2]/div/div/div[2]/div/div/div/div/span[3]'
+                , locator_type=By.XPATH, timeout=25)
+
+            if not is_element_clicked:
+                logging.error(f'Could not switch to History tab')
                 return False
             else:
                 return True
+
         except Exception as e:
-            logging.exception(f'An error occurred trying to click_login_button: {e}')
+            logging.exception(f'An error occurred trying to switch to History tab: {e}')
             return False
 
-    def login(self):
-        try:
-            username_entered = self.enter_username()
-            password_entered = self.enter_password()
-            login_btn_clicked = self.click_login_button()
-            if username_entered and password_entered and login_btn_clicked:
-                logging.info('Successfully logged in!')
-                return True
-            else:
-                logging.error(f'Could not login with provided credentials')
-                return False
-        except Exception as e:
-            logging.exception(f'An error occurred trying to login: {e}')
-            return False
 
-    def visit_and_login(self):
-        try:
-            if not self.visit():
-                return False
-            if not self.login():
-                return False
-            else:
-                logging.info('Browser launched successfully!')
-                return True
-        except Exception as e:
-            logging.exception(f'An error occurred trying to visit_and_login: {e}')
+td = TestDriver()
 
+td.visit()
 
-class Main:
-    def __init__(self):
-        self.base_driver = TestDriver()
-        self.login_page_driver = LoginPage(self.base_driver)
+op = OrdersPage(td)
 
+op.switch_to_history_tab()
 
-        self.login_page_driver.visit_and_login()
-
-
-m = Main()
