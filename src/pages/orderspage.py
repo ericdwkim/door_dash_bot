98290046dcc18693f76cc66ad5ad8ca20627f35b
ericdwkim <ericdwkim.io@gmail.com>
2023-09-24 00:55:16 -0500

    fixed: xpath locator for history tab

--- src/pages/orderspage.py
+++ src/pages/orderspage.py
@@ -13,8 +13,8 @@ class OrdersPage(BasePage):
     def switch_to_history_tab(self):
 
         try:
-            is_element_clicked = self.wait_for_find_then_click('//*[@id="MerchantApp"]/div/div/div[1]/div/div[2]/div[2]/div/div/div[2]/div/div/div/div/span[3]'
-, locator_type=By.XPATH, timeout=25)
+            is_element_clicked = self.wait_for_find_then_click('//*[@id="MerchantApp"]/div/div/div[1]/div/div/div[2]/div/div/div[2]/div/div/div/div/span[3]'
+, locator_type=By.XPATH, timeout=10)
 
             if not is_element_clicked:
                 logging.error(f'Could not switch to History tab')
