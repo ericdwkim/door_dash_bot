dabc51e9a26feca2f93c9a22e09db822595ddcf3
ericdwkim <ericdwkim.io@gmail.com>
2023-09-24 00:55:03 -0500

    remove login env var info in shell script

--- scripts/dd.sh
+++ scripts/dd.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 eval "$(conda shell.bash hook)"
 conda activate bots
-export DD_MERCHANT_LOGIN_URL="https://identity.doordash.com/auth?client_id=1643580605860775164&redirect_uri=https%3A%2F%2Fmerchant-portal.doordash.com%2Fauth_callback&scope=*&prompt=none&response_type=code&layout=merchant_web_v2&state=8b164938-6941-4894-99b8-24c290027440&allowRedirect=true&failureRedirect=%2Fmerchant%2Flogin"
-export DEV_LOGIN_PASSWORD="3%rV@c7ixWgYVn"
-export DEV_LOGIN_EMAIL="ekim@txbstores.com"
 export PYTHONPATH="$HOME/workspace/dd-bot/"
 export SLACK_OAUTH_TOKEN="xoxb-4099486963537-5704022177687-ZVzgnbQHqtX2FzO8svV4ziCI"
 echo "PYTHONPATH has been set to: $PYTHONPATH"
-echo "DEV_LOGIN_EMAIL has been set to: $DEV_LOGIN_EMAIL"
-echo "DEV_LOGIN_PASSWORD has been set to: $DEV_LOGIN_PASSWORD"
-echo "DD_MERCHANT_LOGIN_URL has been set to: $DD_MERCHANT_LOGIN_URL"
 
 today=$(date +"%m-%d-%Y")
 
