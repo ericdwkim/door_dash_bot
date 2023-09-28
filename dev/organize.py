e53b72e87976b3ad2b12f1ede8142100075413c5
ericdwkim <ericdwkim.io@gmail.com>
2023-09-27 20:37:08 -0500

    add: organize.py ; main data cleaning logic chain

---
+++ dev/organize.py
@@ -0,0 +1,120 @@
+from orders import orders
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
+import logging
+import pandas as pd
+import re
+import json
+import csv
+
+
+
+def clean_order_text(order):
+    # Define the patterns in a list
+    patterns = [
+        re.compile(r'(Rate Dasher|Learn More|Get help).*?(?=Pick Up Location)', re.DOTALL),
+        re.compile(r'Channel.*?(?=Order Details)', re.DOTALL),
+        re.compile(r'Associated Transactions \(\d+\).*?(?=Transaction #\d+ - Delivery)', re.DOTALL),
+        re.compile(r'Associated Transactions \(\d+\).*?(?=Transaction #\d+ - Pickup)', re.DOTALL)
+
+    ]
+
+    # Apply each pattern
+    for pattern in patterns:
+        order = re.sub(pattern, '', order).strip()
+
+    # Replace newline characters with spaces
+    order = order.replace('\n', ' ')
+
+    return order
+
+
+
+def parse_menu_items(price_as_value):
+    # Initialize a dictionary for the nested order details
+    item_name_to_item_price = {}
+
+    # Regular expression pattern to match an item and its corresponding price
+    pattern = re.compile(r'(.+?) (\$\d+\.\d+)')
+
+    # Find all matches in the string
+    matches = pattern.findall(price_as_value)
+
+    # Loop through all the matches and populate the dictionary
+    for item_name, item_price in matches:
+        item_name_to_item_price[item_name] = item_price
+
+    return item_name_to_item_price
+
+
+def parse_order(order):
+    # Define the known keys with their regex patterns
+    known_keys = [r'\bOrder: \b', r'\bDelivered\b', r'\bCustomer\sPicked\sUp\b', r'\bCancelled\s-\sNot\sPaid\b', r'\bCancelled\s-\sPaid\b', r'\bPick Up Location\b', r'\bOrder Details\b',
+                  r'\bSubtotal\b(?=\s[^a-zA-Z])', r'\bSubtotal\sTax\b', r'Commission \(\d+%\)',
+                  r'\bTotal Customer Refund\b', r'\bEstimated Payout\b', r'\bAssociated Transactions \(\d+%\)',
+                  r'Transaction #\d+ - Delivery']
+
+    # Initialize the dictionary to store our parsed values
+    order_dict = {}
+
+    # Find positions of known keys
+    positions = []
+    for key in known_keys:
+        for m in re.finditer(key, order):
+            positions.append((m.start(), m.end(), m.group()))
+
+    # Sort positions by their start index
+    positions.sort(key=lambda x: x[0])
+    # print(positions)
+
+    # Create segments based on positions
+    segments = []
+
+    for i in range(len(positions)):
+        start = positions[i][1]
+        end = positions[i + 1][0] if i + 1 < len(positions) else len(order)
+        key = positions[i][2]
+        value = order[start:end].strip().split(' ')[0] if 'Estimated Payout' in key else order[start:end].strip()
+        segments.append((key, value))
+
+    # Parse segments into dictionary
+    for key, value in segments:
+        if key == "Order Details":
+            value = parse_menu_items(value)
+        order_dict[key.replace(':', '').strip()] = value
+
+    return order_dict
+
+
+def main_looper_func(orders):
+
+    results = []
+
+    for order in orders:
+        # 1. Get order strings squeaky clean!
+        order_cleaned = clean_order_text(order)
+
+        # 2. Parse each cleaned order string into formatted key/value pairs
+        parsed_order = parse_order(order_cleaned)
+
+        results.append(parsed_order)
+
+    return results
+
+
+
+results = main_looper_func(orders)
+
+# print(results)
+
+def prettify_list_of_dicts(list_of_dicts):
+    return json.dumps(list_of_dicts, indent=4)
+
+
+print(prettify_list_of_dicts(results))
+
+
