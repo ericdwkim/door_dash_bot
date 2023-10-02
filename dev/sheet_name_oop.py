e402038d4ea1fb96602d1ca6735afc598fe8931b
ericdwkim <ericdwkim.io@gmail.com>
2023-10-02 18:10:01 -0500

    finished: sheet_name_handler in oop

--- dev/sheet_name_oop.py
+++ dev/sheet_name_oop.py
@@ -6,14 +6,12 @@ from datetime import datetime
 
 
 class DataMerger:
-    def __init__(self, json_output_filepath):
+    def __init__(self):
         self.master_dataset_file_path = '/Users/ekim/workspace/personal/dd-bot/dev/store_list.xlsx'
-        self.json_output_filepath = json_output_filepath
-        self.master_df = read_masterdataset_excel()
+        self.json_output_filepath = '/Users/ekim/workspace/personal/dd-bot/dev/json.csv'
+        self.master_df = None
         self.order_to_location_df = None
         self.merged_df = None
-        self.order_id_to_site_num = {}
-
 
     def read_masterdataset_excel(self):
         self.master_df = pd.read_excel(self.master_dataset_file_path, index_col=0)
@@ -31,7 +29,6 @@ class DataMerger:
         self.master_df['Zip'] = self.master_df['Zip'].astype(str)
         self.master_df['street_num'] = self.master_df['street_num'].astype(str)
 
-
     def get_masterdataset_df(self):
         self.read_masterdataset_excel()
         self.add_cols_to_masterset_df()
@@ -40,6 +37,7 @@ class DataMerger:
         with open(self.json_output_filepath, 'r') as f:
             orders = json.load(f)
         return orders
+
 
     def order_id_to_pickup_location(self, orders):
         order_id_to_pickup_location = {}
@@ -53,32 +51,33 @@ class DataMerger:
         orders = self.read_orders()
         raw_orders_to_location = self.order_id_to_pickup_location(orders)
         order_to_location_pairs = list(raw_orders_to_location.items())
-        raw_orders_to_location_df = pd.DataFrame(order_to_location_pairs, columns=['order_id', 'pickup_location'])
+        self.order_to_location_df = pd.DataFrame(order_to_location_pairs, columns=['order_id', 'pickup_location'])
 
-        return raw_orders_to_location_df
+    def splitup_pickup_location(self):
+        # Remove all commas
+        self.order_to_location_df['pickup_location'] = self.order_to_location_df['pickup_location'].str.replace(',', '')
 
-    def splitup_pickup_location(self, df):
+        # Remove "USA" substring
+        self.order_to_location_df['pickup_location'] = self.order_to_location_df['pickup_location'].str.replace('USA', '')
+
         pattern = r'(?P<address>^.+?)\s(?P<city>\w+\s*\w*)\s(?P<state>[A-Z]{2})\s(?P<zip_code>\d{5})'
+        df_extracted = self.order_to_location_df['pickup_location'].str.extract(pattern)
 
-        df_extracted = df['pickup_location'].str.extract(pattern)
+        self.order_to_location_df = pd.concat([self.order_to_location_df, df_extracted], axis=1)
 
-        order_to_split_pickup_location_df = pd.concat([df, df_extracted], axis=1)
-
-        return order_to_split_pickup_location_df
-
-    def add_street_num_col(self, df):
+    def add_street_num_col(self):
         street_num_pattern = r'(?P<street_num>\b\d{3,5}\b)'
 
-        addrs_extracted_df = df['address'].str.extract(street_num_pattern)
+        addrs_extracted_df = self.order_to_location_df['address'].str.extract(street_num_pattern)
 
-        order_to_split_pickup_location_and_street_num_df = pd.concat([df, addrs_extracted_df], axis=1)
+        self.order_to_location_df = pd.concat([self.order_to_location_df, addrs_extracted_df], axis=1)
 
-        return order_to_split_pickup_location_and_street_num_df
+
 
     def get_order_to_location_df(self):
-        raw_orders_to_location_df = self.get_raw_order_to_location_df()
-        order_to_split_pickup_location_df = self.splitup_pickup_location(raw_orders_to_location_df)
-        self.order_to_location_df= self.add_street_num_col(order_to_split_pickup_location_df)
+        self.get_raw_order_to_location_df()
+        self.splitup_pickup_location()
+        self.add_street_num_col()
 
 
     def get_merged_and_organized_master_df(self):
@@ -96,12 +95,13 @@ class DataMerger:
         self.get_merged_and_organized_master_df()
 
     def get_order_id_to_site_num(self):
-        self.order_id_to_site_num = self.merged_df.set_index('order_id')['Site #'].to_dict()
+        order_id_to_site_num = self.merged_df.set_index('order_id')['Site #'].to_dict()
+        print(f'*****************************************\n{order_id_to_site_num}\n*****************************************')
 
 
-if __name__ == '__main__':
-    json_output_filepath = '/Users/ekim/workspace/personal/dd-bot/dev/json.csv'
+    def wrapper(self):
+        self.get_merged_df()
+        self.get_order_id_to_site_num()
 
-    data_merger = DataMerger(json_output_filepath)
-    order_id_to_site_num = data_merger.get_order_id_to_site_num()
-    print(order_id_to_site_num)
+data_merger = DataMerger()
+order_to_location_df = data_merger.wrapper()
--- src/utils/sheet_name_handler.py
+++ src/utils/sheet_name_handler.py
@@ -6,6 +6,13 @@ from datetime import datetime
 
 master_dataset_file_path = '/Users/ekim/workspace/personal/dd-bot/dev/store_list.xlsx'
 
+json_output_filepath = '/Users/ekim/workspace/personal/dd-bot/dev/json.csv'
+
+# excel --> dataframe
+def read_masterdataset_excel(file_path):
+    return pd.read_excel(file_path, index_col=0)
+
+
 def add_cols_to_masterset_df(master_df):
 
     # Create pattern for street_num col
@@ -22,18 +29,36 @@ def add_cols_to_masterset_df(master_df):
 
     return master_df
 
-def get_masterdataset_df(filepath):
-    raw_master_df = pd.read_excel(filepath, index_col=0)
+def get_masterdataset_df(file_path):
+    raw_master_df = read_masterdataset_excel(file_path)
     master_df = add_cols_to_masterset_df(raw_master_df)
     return master_df
 
-def map_order_to_location(orders):
+# list of json orders --> order_id : pickup_location --> dataframe
+def read_orders(file_path):
+    with open(file_path, 'r') as f:
+        orders = json.load(f)
+
+    return orders
+
+def order_id_to_pickup_location(orders):
     order_id_to_pickup_location  = {}
     for order in orders:
         order_id = order['Order']
         store_addrs = order['Pick Up Location']
         order_id_to_pickup_location[order_id] = store_addrs
     return order_id_to_pickup_location
+
+def get_raw_order_to_location_df(file_path):
+
+    orders = read_orders(file_path)
+    raw_orders_to_location = order_id_to_pickup_location(orders)
+
+    order_to_location_pairs = list(raw_orders_to_location.items())
+
+    raw_orders_to_location = pd.DataFrame(order_to_location_pairs, columns=['order_id', 'pickup_location'])
+
+    return raw_orders_to_location
 
 
 def splitup_pickup_location(df):
@@ -55,35 +80,54 @@ def add_street_num_col(df):
     return order_to_split_pickup_location_and_street_num_df
 
 
-# wrapper func
-def get_order_to_pickup_location_df(df):
-    order_to_split_pickup_location_df = splitup_pickup_location(df)
+# internal wrapper func
+def _get_order_to_location_df(raw_df):
+    order_to_location_df = splitup_pickup_location(raw_df)
 
-    order_to_split_pickup_location_and_street_num_df = add_street_num_col(order_to_split_pickup_location_df)
+    order_to_location_df = add_street_num_col(order_to_location_df)
 
-    return order_to_split_pickup_location_and_street_num_df
+    return order_to_location_df
+
+# main wrapper func
+def get_order_to_location_df(file_path):
+    raw_orders_to_location = get_raw_order_to_location_df(file_path)
+
+    order_to_location_df = _get_order_to_location_df(raw_orders_to_location)
+
+    return order_to_location_df
 
 
-def get_merged_and_organized_master_df(df1, df2):
+def get_merged_and_organized_master_df(master_df, df2):
 
     # inner join on fields
-    df = pd.merge(df1, df2, left_on=['Zip', 'street_num'], right_on=['zip_code', 'street_num'], how='inner')
+    merged_df = pd.merge(master_df, df2, left_on=['Zip', 'street_num'], right_on=['zip_code', 'street_num'], how='inner')
 
     # clean up unneeded cols
-    df.drop(
+    merged_df.drop(
         ['Site Description', 'City', 'County', 'State', 'address', 'city', 'state', 'Zip', 'zip_code', 'Notes',
          'street_num'], axis=1, inplace=True)
 
     # reorganize cols
-    df = df[['Site #', 'order_id', 'Address', 'pickup_location']]
+    merged_df = merged_df[['Site #', 'order_id', 'Address', 'pickup_location']]
 
-    return df
+    return merged_df
+
+# internal getter
+def get_order_id_to_site_num(df):
+
+    order_id_to_site_num = df.set_index('order_id')['Site #'].to_dict()
+
+    return order_id_to_site_num
+
+# main merger df wrapper func
+def get_merged_df(master_file_path, orders_file_path):
+    master_df = get_masterdataset_df(master_file_path)
+    order_to_location_df = get_order_to_location_df(orders_file_path)
+    merged_df = get_merged_and_organized_master_df(master_df, order_to_location_df)
+    return merged_df
 
 
-def order_id_to_site_num(df):
+merged_df = get_merged_df(master_dataset_file_path, json_output_filepath)
 
-    order_to_site_num_map = df.set_index('order_id')['Site #'].to_dict()
-
-    return order_to_site_num_map
-
-
+order_id_to_site_num = get_order_id_to_site_num(merged_df)
+print(order_id_to_site_num)
