99dfb1364ee2d84f74ae6616358a0be9794356d2
ericdwkim <ericdwkim.io@gmail.com>
2023-10-05 21:22:39 -0500

    add: testing slack webhook & shell scripts

--- scripts/dd.sh
+++ scripts/dd.sh
@@ -1,26 +1,31 @@
-##!/bin/bash
-#eval "$(conda shell.bash hook)"
-#conda activate bots
-#export PYTHONPATH="$HOME/workspace/dd-bot/"
-#export SLACK_OAUTH_TOKEN="xoxb-4099486963537-5704022177687-ZVzgnbQHqtX2FzO8svV4ziCI"
-#echo "PYTHONPATH has been set to: $PYTHONPATH"
-#
-#today=$(date +"%m-%d-%Y")
-#
-## Define the output directories
+#!/bin/bash
+eval "$(conda shell.bash hook)"
+conda activate bots
+export PYTHONPATH="$HOME/workspace/personal/dd-bot/src/"
+export SLACK_OAUTH_TOKEN="xoxb-4099486963537-5704022177687-ZVzgnbQHqtX2FzO8svV4ziCI"
+
+echo "PYTHONPATH has been set to: $PYTHONPATH"
+
+today=$(date +"%m-%d-%Y")
+
+# Define the output directories
+stdout_dir="/Users/ekim/workspace/txb/dd-bot/dev/build/logs/stdout"
+stderr_dir="/Users/ekim/workspace/txb/dd-bot/dev/build/logs/stderr"
+
+# prod stdout/stderr
 #stdout_dir="C:/Users/bots/workspace/doordash/stdout"
 #stderr_dir="C:/Users/bots/workspace/doordash/stderr"
-#
-#echo "Redirecting standard output to ${stdout_dir}/${today}_out.log"
-#echo "Redirecting standard error to ${stderr_dir}/${today}_err.log"
-#
-## Print the current working directory
-#echo "The current working directory is: $(pwd)"
-#
-## Run the entrypoint python script and redirect stdout and stderr
-##python -m src.test.main > "${stdout_dir}/${today}_out.log" 2> "${stderr_dir}/${today}_err.log"
-#
-#echo "Redirection complete. Check ${stdout_dir}/${today}_out.log and ${stderr_dir}/${today}_err.log for logs."
-#
-#echo "Posting stderr and stdout to Slack channel"
-##python /c/Users/ekima/workspace/dd-bot/scripts/post_to_slack.py
+
+echo "Redirecting standard output to ${stdout_dir}/${today}_out.log"
+echo "Redirecting standard error to ${stderr_dir}/${today}_err.log"
+
+# Print the current working directory
+echo "The current working directory is: $(pwd)"
+
+# Run the entrypoint python script and redirect stdout and stderr
+python -m src.app.__main__ > "${stdout_dir}/${today}_out.log" 2> "${stderr_dir}/${today}_err.log"
+
+echo "Redirection complete. Check ${stdout_dir}/${today}_out.log and ${stderr_dir}/${today}_err.log for logs."
+
+echo "Posting stderr and stdout to Slack channel"
+python /c/Users/ekima/workspace/dd-bot/scripts/post_to_slack.py
---
+++ scripts/post_to_slack.py
@@ -0,0 +1,76 @@
+import requests
+import os
+import datetime
+
+today = datetime.date.today().strftime('%m-%d-%Y')
+
+stdout_file_path=f"/Users/ekim/workspace/txb/dd-bot/dev/build/logs/stdout/${today}_out.log"
+stderr_file_path=f"/Users/ekim/workspace/txb/dd-bot/dev/build/logs/stderr/${today}_err.log"
+
+# TODO
+# File paths stage
+# stdout_file_path = f'C:\\Users\\ekima\\workspace\\DTN BOT\\stdout\\{today}_out.log'
+# stderr_file_path = f'C:\\Users\\ekima\\workspace\\DTN BOT\\stderr\\{today}_err.log'
+
+# File paths prod
+# stdout_file_path = f'C:\\Users\\bots\\workspace\\DTN BOT\\stdout\\{today}_out.log'
+# stderr_file_path = f'C:\\Users\\bots\\workspace\\DTN BOT\\stderr\\{today}_err.log'
+
+# OAuth token with `files:write`
+slack_token = os.getenv('SLACK_OAUTH_TOKEN')
+
+def post_to_slack(file_path, slack_token):
+    """Upload a file to a Slack channel."""
+
+    # Check if file exists
+    if not os.path.isfile(file_path):
+        print(f"The file {file_path} does not exist.")
+        return
+
+    # Extract file name from the path
+    file_name = os.path.basename(file_path)
+    title = f"*{file_name}*"
+
+    # Slack's files.upload endpoint
+    upload_url = "https://slack.com/api/files.upload"
+
+    headers = {
+        'Authorization': f"Bearer {slack_token}",
+    }
+
+    # Use the file's content to create a payload for the POST request
+    with open(file_path, "rb") as file:
+        files = {
+            'file': (file_name, file),
+        }
+        data = {
+            'channels': '#dd_bot',
+            'title': title,
+            'filename': file_name,
+        }
+
+        response = requests.post(upload_url, headers=headers, data=data, files=files)
+
+    # Check response
+    json_response = response.json()
+    if not json_response.get("ok"):
+        raise ValueError(f'Error uploading file to Slack: {json_response.get("error")}')
+
+
+# Upload stdout and stderr to Slack #dtn_bot channel
+def main():
+    # Always post stderr first
+    try:
+        post_to_slack(stderr_file_path, slack_token)
+    except Exception as e:
+        print(f"Error posting stderr file: {e}")
+
+    # Then post stdout
+    try:
+        post_to_slack(stdout_file_path, slack_token)
+    except Exception as e:
+        print(f"Error posting stdout file: {e}")
+
+
+if __name__ == "__main__":
+    main()
